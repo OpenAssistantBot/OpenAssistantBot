1ae587a6f936a6363708d6833cdf2d7b18635c30
ulmaridae <lmaridae@icloud.com>
2020-10-24 15:38:35 +0300

    added question convertor method

--- src/main/java/com/github/openboot/converter/QuestionConverter.java
+++ src/main/java/com/github/openboot/converter/QuestionConverter.java
@@ -2,11 +2,18 @@ package com.github.openboot.converter;
 
 import com.github.openboot.dto.QuestionDto;
 import com.github.openboot.model.Question;
+import com.github.openboot.model.Step;
+
+import java.util.stream.Collectors;
 
 public class QuestionConverter {
 
     public static QuestionDto convertFromModelToDto(Question question) {
-        // TODO: convert Question to QuestionDto
-        return null;
+        int count = 1;
+        StringBuilder answer = new StringBuilder();
+        for (Step step : question.getSteps()) {
+            answer.append(count++).append(". ").append(step.getValue());
+        }
+        return new QuestionDto() {{ setAnswer(answer.toString()); }};
     }
 }
