fb156508c232a2452ea336c593206273c92162f5
egnaf <egnaf@yahoo.com>
2020-10-24 15:18:00 +0300

    fix docker, add swagger, fix service

--- src/main/java/com/github/openboot/controller/BotController.java
+++ src/main/java/com/github/openboot/controller/BotController.java
@@ -1,17 +1,30 @@
 package com.github.openboot.controller;
 
 import com.github.openboot.dto.BotResponseDto;
+import com.github.openboot.model.Question;
+import com.github.openboot.service.QuestionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/bot")
 public class BotController {
+
+    private final QuestionService questionService;
 
     @GetMapping
     public BotResponseDto ask(String question) {
         //...
-        return new BotResponseDto();
+        Question storedQuestion = questionService.search(question);
+
+        BotResponseDto response = new BotResponseDto();
+
+        if (storedQuestion != null) {
+            response.setAnswer(storedQuestion.getQuestion() + "\n" + storedQuestion.getSteps());
+        }
+        return response;
     }
 }
--- src/main/java/com/github/openboot/repository/QuestionRepository.java
+++ src/main/java/com/github/openboot/repository/QuestionRepository.java
@@ -8,4 +8,6 @@ import java.util.UUID;
 
 @Repository
 public interface QuestionRepository extends MongoRepository<Question, UUID> {
+
+    Question findByQuestion(String question);
 }
