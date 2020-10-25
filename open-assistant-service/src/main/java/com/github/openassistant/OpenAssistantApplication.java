1abee2aff9e308520e48d6a988e2740091704ae5
ulmaridae <lmaridae@icloud.com>
2020-10-25 09:38:43 +0300

    added command lime runner

--- open-assistant-service/src/main/java/com/github/openassistant/OpenAssistantApplication.java
+++ open-assistant-service/src/main/java/com/github/openassistant/OpenAssistantApplication.java
@@ -1,7 +1,10 @@
 package com.github.openassistant;
 
+import com.github.openassistant.service.CsvParserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OpenAssistantApplication {
@@ -10,4 +13,8 @@ public class OpenAssistantApplication {
         SpringApplication.run(OpenAssistantApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner dataLoader(CsvParserService service) {
+        return args -> service.initDatabase();
+    }
 }
