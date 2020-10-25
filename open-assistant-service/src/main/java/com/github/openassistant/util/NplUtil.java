03e328bd950f8505b36b25ec328fbd2586b1556d
egnaf <egnaf@yahoo.com>
2020-10-25 10:37:54 +0300

    Fix hostname

--- open-assistant-service/src/main/java/com/github/openassistant/util/NplUtil.java
+++ open-assistant-service/src/main/java/com/github/openassistant/util/NplUtil.java
@@ -2,5 +2,5 @@ package com.github.openassistant.util;
 
 public class NplUtil {
 
-    public static final String URI = "http://0.0.0.0:5000/?question=";
+    public static final String URI = "http://127.0.0.1:5000/?question=";
 }
